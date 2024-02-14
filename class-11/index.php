e3e98638eb3bfe482fd8836707270174ce94c9a7
mdarmanbd <md.armantext@gmail.com>
2024-02-14 23:21:52 +0600

    new class add

--- class-11/index.php
+++ class-11/index.php
@@ -1,25 +1,59 @@
+
+<?php
+    if(isset($_POST['submitMe'])){
+        $number1 = $_POST['number1'];
+        $number2 = $_POST['number2'];
+
+        if($_POST['submitMe'] == 'Add'){
+            $result = $number1 + $number2;
+            echo $result;
+        }
+
+        if($_POST['submitMe'] == 'Sub'){
+            $result = $number1 - $number2;
+            echo $result;
+        }
+
+        if($_POST['submitMe'] == 'Mul'){
+            $result = $number1 * $number2;
+            echo $result;
+        }
+
+        if($_POST['submitMe'] == 'Div'){
+            $result = $number1 / $number2;
+            echo $result;
+        }
+       
+    }   
+
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html>
 <body>
 
-    <!-- <form action="result.php" method="GET">
-        <label for="searchText">Search: </label>
-        <input type="text" name="searchText">
+    <form action="index.php" method="POST">
+        <div style ='padding:20px'>
+            <label for="number1">Number 1</label>
+            <input type="number" name="number1">
+        </div>
 
-        <label for="searchText">location: </label>
-        <input type="text" name="location">
+        <div style ='padding:20px'>
+            <label for="number2">Number 2</label>
+            <input type="number" name="number2">
+        </div>
 
-        <input type="submit" value="serach">
-    </form>  -->
-
-    <form action="result.php" method="POST">
-        <label for="searchText">Num1: </label>
-        <input type="number" name="number1">
-
-        <label for="searchText">Num2: </label>
-        <input type="number" name="number2">
-
-        <input type="submit" value="calculation">
+        <div style ='padding:20px'>            
+            <input type="submit" name="submitMe" value="Add">
+            <input type="submit" name="submitMe" value="Sub">
+            <input type="submit" name="submitMe" value="Mul">
+            <input type="submit" name="submitMe" value="Div">
+        </div>
     </form>
 
 </body>
